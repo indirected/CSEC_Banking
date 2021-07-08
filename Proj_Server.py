d942d267a1ebafeb4c9754e12be4d6f17565562f
indirected <46082152+indirected@users.noreply.github.com>
2021-07-09 01:55:28 +0430

    Added Withdraw, Deposit, Join to accounts

--- Proj_Server.py
+++ Proj_Server.py
@@ -50,6 +50,7 @@ class user:
             user_passhash_dict[username] = (hashlib.sha256((password + salt).encode('ascii')).hexdigest(), salt)
             f = open(user_passhash_filename, 'w')
             json.dump(user_passhash_dict, f, indent=4)
+            f.close()
         #print(user_passhash_dict)
 
 
@@ -62,7 +63,6 @@ class account:
     __pendinglist = []
     __accountnumber = 1000000001
 
-    
     def __init__(self, ownerusername, accounttype, initialamount, conf_lvl, integrity_lvl):
         self.__acounttype = accounttype
         self.__balance = initialamount
@@ -74,6 +74,77 @@ class account:
         #set account number
         if accounts_dict:
             self.__accountnumber = list(accounts_dict)[-1] + 1
+        with accounts_lock:
+            accounts_dict[self.__accountnumber] = self
+            f = open(accounts_filename, 'w')
+            json.dump(accounts_dict, f, indent=4)
+            f.close()
+
+    def Withdraw(self, user, amount):
+        if user in list(self.__userlist):
+            if self.__userlist[user][0].value <= self.__conf_lvl.value and self.__userlist[user][1].value >= self.__integrity_lvl.value:
+                if self.__balance >= amount:
+                    with accounts_lock:
+                        self.__balance -= amount
+                        f = open(accounts_filename, 'w')
+                        json.dump(accounts_dict, f, indent=4)
+                        f.close()
+                    return 1 #Success
+                else: return 0 #Insufficient balance
+        else: return -1 #Access Denied
+
+    
+    def Intake(self, amount):
+        with accounts_lock:
+            self.__balance += amount
+            f = open(accounts_filename, 'w')
+            json.dump(accounts_dict, f, indent=4)
+            f.close()
+
+    def Deposit(self, user, destination, amount):
+        if user in list(self.__userlist):
+            if self.__userlist[user][0].value <= self.__conf_lvl.value and self.__userlist[user][1].value >= self.__integrity_lvl.value:
+                if self.__balance >= amount:
+                    with accounts_lock:
+                        self.__balance -= amount
+                        f = open(accounts_filename, 'w')
+                        json.dump(accounts_dict, f, indent=4)
+                        f.close()
+                    accounts_dict[destination].Intake(amount)
+                    return 1 #Success
+                else: return 0 #Insufficient Balance
+        else: return -1 #Access Denied
+    
+
+    def JoinRequest(self, user):
+        if user in self.__pendinglist:
+            return -1 #User Already in Pending list
+        if user in list(self.__userlist):
+            return 0 #User Already in Accepted List
+        with accounts_lock:
+            self.__pendinglist.append(user)
+            f = open(accounts_filename, 'w')
+            json.dump(accounts_dict, f, indent=4)
+            f.close()
+
+    def AcceptRequest(self, caller, user, conf_lvl, integrity_lvl):
+        if user in self.__pendinglist and caller == self.__owner:
+            with accounts_lock:
+                self.__pendinglist.remove(user)
+                self.__userlist[user] = (conf_lvl, integrity_lvl)
+                f = open(accounts_filename, 'w')
+                json.dump(accounts_dict, f, indent=4)
+                f.close()
+
+    def PrintAccountInfo(self, user):
+        if user in list(self.__userlist):
+            if self.__userlist[user][0].value >= self.__conf_lvl.value and self.__userlist[user][1].value <= self.__integrity_lvl.value:
+                #TODO Print Account info
+                pass
+
+            
+
+
 
         
 
@@ -86,16 +157,20 @@ class account:
 
 
 
-print(RandomSubstring(alphabet+ALPHABET+digits, 10))
-o = user(1,"21")
-o = user(2,"22")
-listt = {'alo':1}
-f = open("file", "w")
-#pickle.dump(listt, f)
-json.dump(user_passhash_dict, f, indent=4)
-f.close()
-f = open("file", "r")
+# print(RandomSubstring(alphabet+ALPHABET+digits, 10))
+# o = user(1,"21")
+# o = user(2,"22")
+# listt = {'alo':1}
+# f = open("file", "w")
+# #pickle.dump(listt, f)
+# json.dump(user_passhash_dict, f, indent=4)
+# f.close()
+# f = open("file", "r")
 
-#list2 = pickle.load(f)
-list2 = json.load(f)
-print(list(list2.values()))
+# #list2 = pickle.load(f)
+# list2 = json.load(f)
+# print(list(list2.values()))
+a = Confidentiality_lvl_List.Secret
+b = Confidentiality_lvl_List.Unclassified
+print(a.value >= b.value)
+print(b)
